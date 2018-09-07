5e9cbf6e4491b20f6e450fd5398bf30902017b44
fhajdarp <ferhat.hajdarpasic@aon.com>
2018-09-07 17:18:27 +1000

    First calls to google api.

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
+//export GOOGLE_APPLICATION_CREDENTIALS=/home/ferhat/carbonblue/carbonblue-7fb6bb02ce68.json
 var http = require('http');
+const {google} = require('googleapis');
+let privatekey = require("./carbonblue-7fb6bb02ce68.json");
 
-var server = http.createServer(function(request, response) {
+async function main() {
+    const auth = await google.auth.getClient({
+        // Scopes can be specified either as an array or as a single, space-delimited string.
+        scopes: ['https://www.googleapis.com/auth/compute',
+        'https://www.googleapis.com/auth/spreadsheets.readonly']
+      });
+    const project = await google.auth.getDefaultProjectId();
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    let token = await auth.getAccessToken();
+    const sheets = google.sheets({version: 'v4', auth});
 
-});
+    var request = {
+      resource: {
+        properties: {
+          title: 'FREDDY'
+        },
+        sheets: [
+          {
+            properties: {
+              title: 'Data',
+              gridProperties: {
+                columnCount: 6,
+                frozenRowCount: 1
+              }
+            }
+          },
+          // TODO: Add more sheets.
+        ]
+      }
+    };
+    let result = await sheets.spreadsheets.create(request, function(err, response) {
+      if (err) {
+        console.log(err);
+      }
+      var spreadsheet = response.data;
+      // TODO: Add header rows.
+      console.log(response);
+    });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+    
+    let count = 0;
+}
 
-console.log("Server running at http://localhost:%d", port);
+main();
